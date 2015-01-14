5877fa071fd85af9609da414debbae91fc4d97b1
Anastasiia Fomenko <nastya@Anastasiias-MacBook-Air.local>
2015-01-14 10:27:09 +0300

    Green integration test on successful edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,13 +21,13 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
-    
   end
   
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #Handle successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
