dcd33b8ab7730ae7d35ba138c1407b7c1476adf8
Anastasiia Fomenko <nastya@Anastasiias-MacBook-Air.local>
2015-02-25 13:53:15 +0300

    Exercises

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -32,6 +32,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal @user.name, name
     assert_equal @user.email, email
+    
+    get login_path
+    log_in_as(@user)
+    assert_redirected_to user_path(@user)
   end
   
   
