1207a73e6a59dbdf92122eceae3530025b5f77e7
Anastasiia Fomenko <nastena-skid@yandex.ru>
2015-11-11 17:54:46 +0300

    ignore public uploads

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       #Configuration for Amazon S3
-      :provider => 'AWS'
+      :provider => 'AWS',
       :aws_access_key_id => ENV['AKIAJ4GYO7MYMQNEYDNA'],
       :aws_secret_access_key => ENV['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']
     }
