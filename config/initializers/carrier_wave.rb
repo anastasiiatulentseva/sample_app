ff4680e70e90e71552c97e272c8d8d81542b1161
Anastasiia Fomenko <nastena-skid@yandex.ru>
2015-11-11 18:26:26 +0300

    change S3 credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       #Configuration for Amazon S3
       :provider => 'AWS',
-      :aws_access_key_id => ENV['AKIAJ4GYO7MYMQNEYDNA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_ACCESS_KEY']
     }
-    config.fog_directory = ENV['atulentseva']
+    config.fog_directory = ENV['S3_ACCESS_KEY']
   end
 end
